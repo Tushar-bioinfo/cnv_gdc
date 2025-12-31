a402457597283c37d3db8f172afd6c7314de3d86
Tushar Singh [st25] <st25@rra-login1.rc.usf.edu>
2025-12-30 19:50:18 -0500

    SEPTIN14 computed

--- gdcsclice.sh
+++ gdcsclice.sh
@@ -43,6 +43,6 @@ NAME=$(cut -d',' -f2 <<< $InputString)
 
 echo "[$(date)] Working on -> $NAME "
 
-APItext="https://api.gdc.cancer.gov/slicing/view/$ID?region=chr7:56011051-56051604" #PSPH
+APItext="https://api.gdc.cancer.gov/slicing/view/$ID?region=chr7:55793540-55862755" #SEPTIN14
 token=$(<$Token)
 curl --header "X-Auth-Token: $token" $APItext --output $OutputFolder/sliced_$NAME
